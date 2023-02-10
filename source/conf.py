82136d1c15652dfa02d942492312a7ab9a790745
jennyhuang17 <jiehuang1217@gmail.com>
2023-02-10 12:22:00 +0800

    updated the copyright info in conf.py

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,7 @@
 ##############################################################################
 
 project = 'Red Hat Ansible Certified Content for IBM Z'
-copyright = 'IBM Corporation 2020, 2021'
+copyright = 'IBM Corp. 2020, 2023'
 author = 'IBM'
 
 # Disable the Copyright footer for Read the docs at the bottom of the page
